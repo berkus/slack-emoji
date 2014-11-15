c497d0594971f6613decb79ad34737a0c1ae96d8
Berkus Decker <berkus@madfire.net>
2014-11-15 13:03:26 +0200

    Add per-team emoji names.

--- getemoji.sh
+++ getemoji.sh
@@ -3,7 +3,8 @@
 
 while read url; do
     echo $url
+    TEAMNAME=$(echo $url | awk -F'/' '{print $3}' | cut -d . -f 1)
     BASENAME=$(echo $url | awk -F'/' '{print $5}')
     EXTNAME="${url##*.}"
-    wget "$url" -O "$BASENAME.$EXTNAME" -o /dev/null
+    wget "$url" -O "emoji/$TEAMNAME-$BASENAME.$EXTNAME" -o /dev/null
 done < "allemoji.txt"
